35ab3e724e7fce43b6ab986c646fc2dba2f13043
marcos3062011-web <marcos3062011@gmail.com>
2026-06-21 21:57:37 -0300

    Login screen + firebase

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,92 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonInput, IonItem, IonCardTitle, IonButton, IonFab, IonFabButton, IonCardContent, IonIcon, IonCardHeader } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { getAuth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, deleteUser } from 'firebase/auth';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonCardTitle, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonInput, IonItem, IonButton, FormsModule, IonFab, IonFabButton, IonCardContent, IonIcon, IonCardHeader ]
 })
 export class Tab2Page {
+  emailUser: string = '';
+  passwordUser: string = '';
+
+  private auth = getAuth();
 
   constructor() {}
 
+  register() {
+    if (!this.emailUser || !this.passwordUser) {
+      alert('Please insert Email and Password.');
+      return;
+    }
+
+    createUserWithEmailAndPassword(this.auth, this.emailUser, this.passwordUser)
+      .then((userCredential) => {
+        alert('Account: ' + userCredential.user.email + ' created.');
+        this.emailUser = '';
+        this.passwordUser = '';
+      })
+      .catch((error) => {
+        console.error('Register Error:', error.code);
+        if (error.code === 'auth/email-already-in-use') {
+          alert('e-mail already in use.');
+        } else if (error.code === 'auth/weak-password') {
+          alert('password must have at least 6 characters.');
+        } else {
+          alert('Error:' + error.message);
+        }
+      });
+  }
+
+  login() {
+    if (!this.emailUser || !this.passwordUser) {
+      alert('Please insert Email and Password.');
+      return;
+    }
+    signInWithEmailAndPassword(this.auth, this.emailUser, this.passwordUser)
+      .then((userCredential) => {
+        alert('Succesful LogIn: ' + userCredential.user.email);
+        this.emailUser = '';
+        this.passwordUser = '';
+      })
+      .catch((error) => {
+        if (error.code === 'auth/invalid-credential') {
+          alert('Invalid E-mail or Password.');
+        } else {
+          alert('LogIn Error: ' + error.message);
+        }
+      });
+  }
+
+  logout() {
+    signOut(this.auth)
+      .then(() => alert('Successful LogOff.'))
+      .catch((error) => alert('LogOff Error: ' + error.message));
+  }
+
+  deleteAccount() {
+    const user = this.auth.currentUser;
+
+    if (!user) {
+      alert('You need to login if you would like to delete your account.');
+      return;
+    }
+    if (confirm('Are you sure you want to delete?.')) {
+      deleteUser(user)
+        .then(() => {
+          alert('Account Deleted.');
+        })
+        .catch((error) => {
+          console.error('Delete Error:', error.code);
+          if (error.code === 'auth/requires-recent-login') {
+            alert('Not Found.');
+          } else {
+            alert('Delete Error: ' + error.message);
+          }
+        });
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,22 @@ import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZfTQ2tAMFiGvo5aUDAeSVvq3WOsUXEBA",
+  authDomain: "projeto-sos-df03a.firebaseapp.com",
+  projectId: "projeto-sos-df03a",
+  storageBucket: "projeto-sos-df03a.firebasestorage.app",
+  messagingSenderId: "57830393070",
+  appId: "1:57830393070:web:3547d48d0a2ba3f40e5fff",
+  measurementId: "G-3R6FHDFMG3"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
